ee74afc4ca0d8d6d6d9bf496d1a430a5affd2564
molai.dev <mahir@molai.dev>
2021-07-28 12:00:13 -0700

    update

--- components/Logo.vue
+++ components/Logo.vue
@@ -3,14 +3,4 @@
 </template>
 
 <style>
-.NuxtLogo {
-  animation: 1s appear;
-  margin: auto;
-}
-
-@keyframes appear {
-  0% {
-    opacity: 0;
-  }
-}
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,32 +1,6 @@
 <template>
 <div class="container">
-    <section class="main">
-      <h1 class="heading"><b>Mahir Molai</b></h1>
-      <br>
-      <h5 class="white alilbig">I'm a full-stack developer, graphic designer, animator and writer.</h5>
-      <br>
-      <h5 class="white proj">Projects</h5>
-      <br>
-      <ul class="underproj">
-        <li><a class="white" href="https://bot.molai.dev">MolaiBOT</a></li>
-        <li><a class="white" href="https://freedom.molai.dev">Freedom App</a></li>
-      </ul>
-      <br>
-      <a href="https://instagram.com/molai.dev" class="white ig">
-        <i class="fab fa-instagram"></i>
-      </a>
-      <br>
-      <a href="https://discord.com/users/763767239018938368" class="white disc">
-        <i class="fab fa-discord"></i>
-      </a>
-      <br>
-      <a class="white git" href="https://github.com/mtgsquad">
-        <i class="fab fa-github"></i>
-      </a>
-    </section>
-    <div class="footer">
-      <p class="white"><a class="white link" href="/">Mahir Molai</a> © 2021</p>
-    </div>
+  <h1 class="heading"><b>Mahir Molai</b></h1>
 </div>
 </template>
 
@@ -56,120 +30,23 @@
 .container{
   font-family: "Poppins", sans-serif;
   height: 100vh;
-  background: #000115;
-  cursor: url('./img/cursor3.svg'), auto;
+  /*background: #000115;*/
   position: relative;
   margin: 0;
   padding: 0;
   box-sizing: border-box;
-}
-
-.main{
-  height: 90vh;
-  position: relative;
+  background-image: url('../assets/bg.gif');
+  background-repeat: repeat;
 }
 
 .heading{
   position: absolute;
-  top: 30%;
+  top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-  color: rgba(255, 255, 255, 0.8);
-  font-size: 65px;
-  transition: 0.3s ease-in;
+  color: #ffffff;
+  font-size: 74px;
+  transition: 0.7s ease;
   text-decoration: none;
-}
-
-.heading:hover{
-  color: orange;
-  text-decoration: underline;
-  cursor: url('./img/cursor4.svg'), auto;
-}
-
-h5:hover {
-  cursor: url('./img/cursor4.svg'), auto;
-  text-decoration: underline;
-  color: orange;
-}
-
-.alilbig {
-  margin-left: 30px;
-  position: absolute;
-  top: 45%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  font-size: 28px;
-}
-
-.footer {
-  top: 92%;
-  left: 50%;
-  position: absolute;
-  transform: translate(-50%, -50%);
-}
-
-.link:hover {
-  cursor: url('./img/cursor4.svg'), auto;
-}
-
-.ig {
-  position: absolute;
-  top: 89%;
-  left: 45%;
-  transform: translate(-50%, -50%);
-  font-size: 30px;
-  transition: 0.3s ease-in-out;
-}
-
-.ig:hover {
-  color: orange;
-  cursor: url('./img/cursor4.svg'), auto;
-}
-
-.disc {
-  position: absolute;
-  top: 89%;
-  left: 55%;
-  transform: translate(-50%, -50%);
-  font-size: 30px;
-  transition: 0.3s ease-in-out;
-}
-
-.disc:hover {
-  color: orange;
-  cursor: url('./img/cursor4.svg'), auto;
-}
-
-.git {
-  position: absolute;
-  top: 89%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  font-size: 30px;
-  transition: 0.3s ease-in-out;
-}
-
-.git:hover {
-  color: orange;
-  cursor: url('./img/cursor4.svg'), auto;
-}
-
-.proj {
-  margin-left: 30px;
-  position: absolute;
-  top: 57%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  font-size: 28px;
-}
-
-.underproj {
-  margin-left: 30px;
-  position: absolute;
-  top: 67%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  font-size: 28px;
-  color: white;
 }
 </style>
