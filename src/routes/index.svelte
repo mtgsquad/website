de5de2c366069a0709b9999386bc95671e733b61
Mahir Molai <mahir@molai.dev>
2022-08-03 23:36:55 +0500

    updateeee

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -133,14 +133,17 @@
             border: none;
             border-radius: 10px;
             box-shadow: 0 0 15px rgba(0, 0, 0, 0.1);
-            transition: 600ms ease-in-out;
+            transition: 450ms ease-in-out;
             box-shadow: 10px 10px 0px 0px $shadow;
             -webkit-box-shadow: 10px 10px 0px 0px $shadow;
             -moz-box-shadow: 10px 10px 0px 0px $shadow;
             &:hover {
-                box-shadow: 25px 25px 0px 0px $shadow;
-                -webkit-box-shadow: 25px 25px 0px 0px $shadow;
-                -moz-box-shadow: 25px 25px 0px 0px $shadow;
+                box-shadow: 10px 10px 0px 0px $btn;
+                -webkit-box-shadow: 10px 10px 0px 0px $btn;
+                -moz-box-shadow: 10px 10px 0px 0px $btn;
+                background: $btn-text-colour;
+                color: $btn;
+                border: 2px solid $btn;
             }
         }
 
@@ -166,15 +169,17 @@
                 border: none;
                 border-radius: 10px;
                 box-shadow: 0 0 15px rgba(0, 0, 0, 0.1);
-                transition: 600ms ease-in-out;
+                transition: 450ms ease-in-out;
                 box-shadow: 10px 10px 0px 0px $shadow;
                 -webkit-box-shadow: 10px 10px 0px 0px $shadow;
                 -moz-box-shadow: 10px 10px 0px 0px $shadow;
-
                 &:hover {
-                    box-shadow: 25px 25px 0px 0px $shadow;
-                    -webkit-box-shadow: 25px 25px 0px 0px $shadow;
-                    -moz-box-shadow: 25px 25px 0px 0px $shadow;
+                    box-shadow: 10px 10px 0px 0px $btn;
+                    -webkit-box-shadow: 10px 10px 0px 0px $btn;
+                    -moz-box-shadow: 10px 10px 0px 0px $btn;
+                    background: $btn-text-colour;
+                    color: $btn;
+                    border: 2px solid $btn;
                 }
             }
         }
