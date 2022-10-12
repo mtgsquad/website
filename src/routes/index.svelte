3047441562898627b193eb07fe17bc96f6911489
Mahir Molai <mahir@molai.dev>
2022-10-12 20:56:50 +0500

    Updates

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,35 +2,34 @@
     export let prerender = true;
 </script>
 
-<script>
+<script lang="ts">
     import { onMount } from 'svelte';
+    import ArrowDown from "carbon-icons-svelte/lib/ArrowDown.svelte";
+    import LogoGithub from "carbon-icons-svelte/lib/LogoGithub.svelte";
 
-    /**
-    * @type {HTMLDivElement}
-    */
-    let header;
-    
+    let header: HTMLDivElement;
 
-    /**
-    * @type {HTMLElement}
-    */
-    let credits;
+    let credits: HTMLElement;
+
+    let nav: HTMLElement;
+
+    let scrollY: number;
 
     onMount(()=> {
         let shadow = "";
 
-        for(var i = 0; i < 1000; i++) {
+        for(var i = 0; i < 750; i++) {
             shadow +=(shadow? ",":"")+ i*1+"px "+ i*1+'px 0 #eece1b'
         }
 
         header.style.textShadow = shadow;
-
-        // musicPlayer.play();
-    })
+    });
 </script>
 
+<svelte:window bind:scrollY={scrollY}/>
+
 <main id="top">
-    <nav>
+    <nav class={scrollY > 50 ? "scrolled" : ""} >
         <button class="btn" on:click={()=>window.location.href = "#top"}>molai.dev</button>
         <div class="links">
             <button on:click={()=>window.location.href = "mailto:mahir@molai.dev"}>Email</button>
@@ -41,33 +40,30 @@
         <p class="design">Designer</p>
         Mahir Molai
         <p class="frontend">Front-End Dev</p>
+        {#if scrollY < 50}
+        <div on:click={() => {
+            scrollY = 500;
+            console.log(scrollY);
+        }} class="showScroller">
+            <ArrowDown size={48} />
+            <h4>Scroll Down</h4>
+        </div>
+        {/if}
     </div>
-    <!--
-        <div class="player">
-        <p bind:this={credits} class="credits">Music Credits:
+    <section class="about">
+        <h1>About</h1>
+        <p>My name is Mahir Molai & I'm a web developer from Pakistan 🇵🇰
             <br>
-            <a href="https://www.youtube.com/watch?v=mMYfOB_rzac" target="_blank">Keshi - Beside You</a>
-       </p>
-       
-        <audio bind:this={musicPlayer} class="gap-bottom-left" controls src="./music/bgsong.mp3">
-       </audio>
-    </div>
-    -->
-    <div class="ad">
-        <amp-ad
-            layout="fixed"
-            width="120"
-            height="120"
-            type="adsense"
-            data-ad-client="ca-pub-2703289420312218"
-            data-ad-slot="6837791691">
-        </amp-ad>
-    </div>
-    <!--<section id="contact" class="contact">
-        <form action="/api/form">
-            <input type="text">
-        </form>
-    </section>-->
+           I only have one hobby, and that is to write code for open source projects.
+        </p>
+        <br>
+        <h2>Socials</h2>
+        <br>
+        <button on:click={() => window.open("https://github.com/mtgsquad")} class="social-btn">
+            <LogoGithub size={32} />
+            GitHub
+        </button>
+    </section>
 </main>
 
 <style lang="scss">
@@ -79,24 +75,23 @@
     $shadow: #eece1b;
     $btn: #b8a01b;
 
-    @keyframes typing {
-        from { width: 0 }
-        to { width: 517px }
-    }
-    @keyframes blink {
-        from { border-color: transparent }
-        to { border-color: orange; }
-    }
     .credits {
         max-width: 250px;
         text-align: center;
         margin-bottom: 15px;
         font-size: 20px;
     }
+
+    .scrolled {
+        box-shadow: 0 2px 10px 0 rgba(0, 0, 0, 0.15);
+        background-color: darken($color: $bg, $amount: 5%);
+    }
+
     main {
         display: flex;
         flex-direction: column;
-        height: 100vh;
+        align-items: center;
+        height: auto;
         width: 100vw;
         scroll-behavior: smooth;
         font-family: 'Roboto Mono', monospace;
@@ -117,6 +112,7 @@
         padding-left: 70px;
         padding-right: 70px;
         position: fixed;
+        transition: 300ms ease-in-out;
 
         .btn {
             display: flex;
@@ -130,11 +126,11 @@
             border-radius: 10px;
             transition: 450ms ease-in-out;
             box-shadow: 10px 10px 0px 0px $btn;
-                -webkit-box-shadow: 10px 10px 0px 0px $btn;
-                -moz-box-shadow: 10px 10px 0px 0px $btn;
-                background: $btn-text-colour;
-                color: $btn;
-                border: 2px solid $btn;
+            -webkit-box-shadow: 10px 10px 0px 0px $btn;
+            -moz-box-shadow: 10px 10px 0px 0px $btn;
+            background: $btn-text-colour;
+            color: $btn;
+            border: 2px solid $btn;
             &:hover {
                 box-shadow: 10px 10px 0px 0px $shadow;
                 -webkit-box-shadow: 10px 10px 0px 0px $shadow;
@@ -187,19 +183,57 @@
         color: $text-colour;
         font-size: 100px;
         width: 100vw;
-        height: calc(100vh - 130px);
+        height: calc(100vh - 130px) !important;
         display: flex;
         align-items: center;
         flex-direction: column;
-        justify-content: center;
         text-align: center;
         text-shadow: 1px 1px 0 $shadow;
         gap: 50px;
+        margin-top: 130px;
+        transition: 300ms ease-in-out;
 
         .frontend {
             font-size: 42px;
             align-self: flex-start;
             margin-left: 230px;
+        }
+
+        div {
+            transition: 100ms ease-in-out;
+        }
+
+        .showScroller {
+            font-size: 24px;
+            margin-top: 275px;
+            border-radius: 15px;
+            width: 275px;
+            background: $btn;
+            height: 70px;
+            padding: 15px;
+            display: flex;
+            align-items: center;
+            justify-content: space-evenly;
+            transition: 450ms ease-in-out;
+            box-shadow: 10px 10px 0px 0px darken($shadow, 6%);
+            -webkit-box-shadow: 10px 10px 0px 0px darken($shadow, 6%);
+            cursor: pointer;
+            color: $btn-text-colour;
+
+            &:hover {
+                box-shadow: 10px 10px 0px 0px $btn;
+                -webkit-box-shadow: 10px 10px 0px 0px $btn;
+                -moz-box-shadow: 10px 10px 0px 0px $btn;
+                background: $btn-text-colour;
+                color: $btn;
+                border: 2px solid $btn;
+            }
+            
+
+            h4 {
+                transition: 300ms ease-in-out;
+                text-shadow: none;
+            }
         }
 
         .design {
@@ -209,33 +243,53 @@
         }
     }
 
-    /**
-            .player {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        gap: 5px;
-        width: 300px;
-        margin-left: 20px;
-        margin-bottom: 20px;
-        align-self: flex-start;
-        justify-self: flex-end;
-        text-shadow: 1px 1px 0 $shadow;
-        color: $text-colour;
+    .about {
+        height: 50vh;
+        padding: 75px;
+        width: 100vw;
+        background-color: darken($color: $bg, $amount: 5%);
+        color: darken($btn, 9%);
+        box-shadow: 0 -5px 5px 0px rgba(0,0,0,0.1);
+        -webkit-box-shadow: 0 -5px 5px 0px rgba(0,0,0,0.1);
+        -moz-box-shadow: 0 -5px 5px 0px rgba(0,0,0,0.1);
 
-        a {
-            color: blue;
-            text-decoration: none;
+        h1 {
+            font-size: 48px;
         }
-    }
-    */
+        h2 {
+            font-size: 36px;
+        }
+        p {
+            font-size: 28px;
+        }
 
-    .ad {
-        background: $bg-ad;
-        width: 150px;
-        height: 150px;
-        align-self: flex-end;
-        justify-self: flex-end;
-        border-radius: 15px 0px 0px 0px;
+        .social-btn {
+            display: flex;
+            font-size: 24px;
+            align-items: center;
+            justify-content: center;
+            gap: 10px;
+            background: $btn;
+            width: 150px;
+            height: 60px;
+            cursor: pointer;
+            color: $btn-text-colour;
+            font-family: 'Roboto Mono', monospace;
+            border: none;
+            border-radius: 10px;
+            box-shadow: 0 0 15px rgba(0, 0, 0, 0.1);
+            transition: 450ms ease-in-out;
+            box-shadow: 10px 10px 0px 0px $shadow;
+            -webkit-box-shadow: 10px 10px 0px 0px $shadow;
+            -moz-box-shadow: 10px 10px 0px 0px $shadow;
+            &:hover {
+                box-shadow: 10px 10px 0px 0px $btn;
+                -webkit-box-shadow: 10px 10px 0px 0px $btn;
+                -moz-box-shadow: 10px 10px 0px 0px $btn;
+                background: $btn-text-colour;
+                color: $btn;
+                border: 2px solid $btn;
+            }
+        }
     }
 </style>
